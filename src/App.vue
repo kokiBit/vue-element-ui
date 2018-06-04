cdaa0a394d5532aac880bfc4138360c7358fc97d
seattle <>
2018-06-04 14:56:35 +0900

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    const item = {
+      date: '2016-05-02',
+      name: 'Tom',
+      address: 'No. 189, Grove St, Los Angeles'
+    };
+    return {
+      tableData: Array(20).fill(item)
+    }
+  }
+}
+</script>
+
+<style>
+.el-header {
+    background-color: #B3C0D1;
+    color: #333;
+    line-height: 60px;
+  }
+  
+  .el-aside {
+    color: #333;
+  }  
+</style>
